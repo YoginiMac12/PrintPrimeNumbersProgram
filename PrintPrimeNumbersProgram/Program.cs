06432bb5ae5908e6e06eafc996fb4e72ae195f2e
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-22 22:59:04 +0530

    Add files via upload

---
+++ PrintPrimeNumbersProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPrimeNumbersProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine("Enter the Numbers:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("is Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("is not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
